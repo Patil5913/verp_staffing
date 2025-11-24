0f7b41a194cc6a1ada2772c18d9500b965849dd0
Uday <uday.sde@vrugle.com>
2025-11-24 10:23:46 +0000

    solved bugs.

--- verp_staffing/crm/doctype/customer/customer.py
+++ verp_staffing/crm/doctype/customer/customer.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2025, Vrugle and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Customer(Document):
-	pass
+	def before_save(self):
+		if self.opportunity:
+			lead = frappe.db.get_value("Opportunity", self.opportunity, "party_name")
+			if lead:
+				self.title = frappe.db.get_value("Lead", lead, "name1")
--- verp_staffing/crm/doctype/opportunity/opportunity.js
+++ verp_staffing/crm/doctype/opportunity/opportunity.js
@@ -134,6 +134,7 @@ frappe.ui.form.on("Opportunity", {
         }
     },
 
+
     setup: function (frm) {
         frm.set_query("opportunity_from", function () {
             return {
@@ -143,7 +144,6 @@ frappe.ui.form.on("Opportunity", {
             };
         });
     },
-
 
     opportunity_from: function (frm) {
         if (frm.doc.opportunity_from) {
