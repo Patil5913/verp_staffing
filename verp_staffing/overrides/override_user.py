63593a382a08898305d302d947d92d019433e334
kihan2518B <kp648027@gmail.com>
2026-01-02 11:57:30 +0000

    After_insert to on_update

--- verp_staffing/overrides/override_user.py
+++ verp_staffing/overrides/override_user.py
@@ -3,8 +3,8 @@ from frappe.core.doctype.user.user import User as FrappeUser
 from frappe import _
 
 class CustomUser(FrappeUser):
-    def after_insert(self):
-        super().after_insert()
+    def on_update(self):
+        super().on_update()
         frappe.errprint(f"CustomUser after_insert called for user: {self.name}")
         frappe.msgprint(
             msg=_("This user does not have an Employee record.\nYou should create an Employee for proper system access."),
