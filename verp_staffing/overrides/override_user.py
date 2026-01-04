1a591061c9fbd0a50aa63f855f5055a6f88dbcff
kihan2518B <kp648027@gmail.com>
2026-01-04 06:03:49 +0000

    Added after_insert in custom user

--- verp_staffing/overrides/override_user.py
+++ verp_staffing/overrides/override_user.py
@@ -25,3 +25,26 @@ class CustomUser(User):
                 "hide_on_success": True,
             },
         )
+    def after_insert(self):
+        frappe.errprint(f"after_insert called for {self.name}")
+        if not getattr(self.flags, "in_insert", False):
+            return
+        if self.name == "Administrator":
+            return
+
+        employee_exists = frappe.db.exists("Employee", {"user": self.name})
+        if employee_exists:
+            return
+
+        frappe.msgprint(
+            msg=_("from onupdate This user does not have an Employee record.\nYou should create an Employee for proper system access."),
+            title=_("Missing Employee Record"),
+            primary_action={
+                "label": _("Create Employee"),
+                "client_action": "frappe.set_route",
+                "args": ["Employee", "new-employee-1"],
+                "hide_on_success": True,
+            },
+        )
+        return super().after_insert()
+        
