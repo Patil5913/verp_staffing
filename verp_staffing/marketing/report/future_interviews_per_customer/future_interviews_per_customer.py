c51380b31a32477bd0e3f2d18bda2476cb89feb6
archit280 <architthakkar1@gmail.com>
2026-02-24 12:11:56 +0000

    change in report 'fututre_inerview_report'

--- verp_staffing/marketing/report/future_interviews_per_customer/future_interviews_per_customer.py
+++ verp_staffing/marketing/report/future_interviews_per_customer/future_interviews_per_customer.py
@@ -1,56 +1,3 @@
-# # Copyright (c) 2026, Vrugle and contributors
-# # For license information, please see license.txt
-
-# import frappe
-# from frappe.utils import getdate
-
-# def execute(filters=None):
-#     filters = filters or {}
-
-#     today = getdate()
-
-#     conditions = " AND ir.date_of_interview > %(today)s"
-#     values = {"today": today}
-
-#     if filters.get("to_date"):
-#         conditions += " AND ir.date_of_interview <= %(to_date)s"
-#         values["to_date"] = filters["to_date"]
-
-#     data = frappe.db.sql("""
-#         SELECT
-#             c.title AS customer,
-#             COUNT(ir.name) AS upcoming_interviews
-#         FROM `tabInterview` i
-#         INNER JOIN `tabInterview Round` ir
-#             ON ir.parent = i.name
-#         INNER JOIN `tabMarketing` m ON m.name = i.marketing_link
-#         INNER JOIN `tabCustomer` c ON c.name = m.customer
-#         WHERE 1=1 {conditions}
-#         GROUP BY c.title
-#         ORDER BY upcoming_interviews DESC
-#     """.format(conditions=conditions), values, as_dict=True)
-
-#     columns = [
-#         {"label": "Customer", "fieldname": "customer", "fieldtype": "Link", "options": "Customer"},
-#         {"label": "Upcoming Interviews", "fieldname": "upcoming_interviews", "fieldtype": "Int"},
-#     ]
-
-#     chart = {
-#         "data": {
-#             "labels": [d.customer for d in data],
-#             "datasets": [
-#                 {
-#                     "name": "Upcoming Interviews",
-#                     "values": [d.upcoming_interviews for d in data],
-#                 }
-#             ],
-#         },
-#         "type": "bar",
-#     }
-
-#     return columns, data, None, chart
-
-
 # Copyright (c) 2026, Vrugle and contributors
 # For license information, please see license.txt
 
@@ -91,7 +38,7 @@ def execute(filters=None):
     data = frappe.db.sql(f"""
         SELECT
             c.name AS customer,
-            c.title AS customer_name,
+            c.title AS name1,
             COUNT(ir.name) AS upcoming_interviews
         FROM `tabInterview` i
         INNER JOIN `tabInterview Round` ir
@@ -116,7 +63,7 @@ def execute(filters=None):
         },
         {
             "label": "Customer Name",
-            "fieldname": "customer_name",
+            "fieldname": "name1",
             "fieldtype": "Data"
         },
         {
@@ -128,7 +75,7 @@ def execute(filters=None):
 
     chart = {
         "data": {
-            "labels": [d.customer_name for d in data],
+            "labels": [d.name1 for d in data],
             "datasets": [
                 {
                     "name": "Upcoming Interviews",
