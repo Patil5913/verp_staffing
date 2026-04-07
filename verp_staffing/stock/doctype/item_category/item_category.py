4bf1539da8d000eec3e59cd60af842ce30bc94f5
kihan2518B <kp648027@gmail.com>
2026-04-07 07:13:50 +0000

    feat: Created items doctype

---
+++ verp_staffing/stock/doctype/item_category/item_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Vrugle and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class ItemCategory(NestedSet):
+	pass
